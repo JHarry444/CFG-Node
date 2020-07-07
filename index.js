f5ed83b4f087af19b3966e50675624c82221046d
Jordan <jordan.harrison@qa.com>
2020-07-07 12:43:14 +0100

    added error handling

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
+app.use(cors());
+
 app.use(bodyParser.json());
+
+app.use(function (request, response, next) {
+    console.log(`Request received at ${new Date()}`);
+    next();
+});
 
 app.get('/', function (request, response) {
     response.status(418).send('Hello, World!');
@@ -25,8 +32,17 @@ app.delete('/remove/:bloop', function (req, res) {
     const params = req.params;
     console.log(params);
     res.status(200).send(params.bloop);
+});
+
+app.get('/error', function (req, res, next) {
+    // throw new Error('I am a contrived error!');
+    return next(new Error('I am a contrived error!'));
+    console.log('?');
 })
 
+app.use(function (error, request, response, next) {
+    response.status(500).send(error.stack);
+});
 
 const server = app.listen(4494, function () {
     console.log(`Server started successfully on port ${server.address().port}`)
